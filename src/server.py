f1ed8e63ef57fe965ad5b1abc4d2bd24e2fd31fd
eric <eric.3126@hotmail.com>
2014-01-20 13:49:40 +0800

    fix thread unexit issue | support python 2.6

--- src/server.py
+++ src/server.py
@@ -13,6 +13,30 @@ from autobahn.twisted.websocket import WebSocketServerFactory, \
                                        listenWS
 
 logQueue = Queue.Queue(maxsize = 100)
+signal_terminate = False
+
+def set_exit_handler(func):
+    import signal
+    signal.signal(signal.SIGINT, func)
+    signal.signal(signal.SIGTERM, func)
+#    signal.signal(signal.SIGKILL, func)
+
+def on_exit(sig, func=None):
+    print "******exit handler triggered******"
+    global signal_terminate
+    signal_terminate = True
+
+class ReadLogThread(threading.Thread):
+    
+    def __init__(self, log_path):
+        threading.Thread.__init__(self)
+        self.log_path = log_path
+    
+    def run(self):
+        for line in tail("-f", self.log_path, _iter=True):
+            if signal_terminate: break
+            print line
+            logQueue.put(line)
 
 class BroadcastServerProtocol(WebSocketServerProtocol):
 
@@ -35,39 +59,34 @@ class BroadcastServerFactory(WebSocketServerFactory):
         self.sendLog()
 
     def sendLog(self):
-         str = ""
-         while not logQueue.empty():
-            str += logQueue.get()
-         if str != "":
-             self.broadcast(str.encode('utf8'))
-         reactor.callLater(1, self.sendLog)
+        if signal_terminate: reactor.stop()
+        str = ""
+        while not logQueue.empty():
+           str += logQueue.get()
+        if str != "":
+            self.broadcast(str.encode('utf8'))
+        reactor.callLater(1, self.sendLog)
 
     def register(self, client):
         if not client in self.clients:
-            print("registered client {}".format(client.peer))
+            print("registered client {0}".format(client.peer))
             self.clients.append(client)
 
     def unregister(self, client):
         if client in self.clients:
-            print("unregistered client {}".format(client.peer))
+            print("unregistered client {0}".format(client.peer))
             self.clients.remove(client)
 
     def broadcast(self, msg):
-        print("broadcasting message '{}' ..".format(msg))
+        print("broadcasting message '{0}' ..".format(msg))
         for c in self.clients:
             c.sendMessage(msg.encode('utf8'))
-            print("message sent to {}".format(c.peer))
-
-def readLog(log_path):
-    for line in tail("-f", log_path, _iter=True):
-        print line
-        logQueue.put(line)
-
+            print("message sent to {0}".format(c.peer))
 
 if __name__ == '__main__':
     '''
     argv[1]为日志绝对路径 必填项
-    argv[2]为服务器端口号 默认未9002    
+    argv[2]为服务器端口号 默认未9003
     网页日志服务器启动方式如下：
         python server.py /home/abc/project/someProject/src/tmp/log/std.log 9002 
     '''
@@ -75,13 +94,13 @@ if __name__ == '__main__':
         print "***Error: Need log_path, i.e. '$python server.py /var/sys.log'***"
         sys.exit(1)
     elif len(sys.argv) <= 2:
-        print "***No port input, 9002 as default***"
+        print "***No port input, 9003 as default***"
         log_path = sys.argv[1]
-        port = '9002'
+        port = '9003'
     else:
         log_path = sys.argv[1]
         port = sys.argv[2]
-        print "***Server start! using port %d***" %port
+        print "***Server start! using port %s***" %port
     print "***Make sure you have modified log.html as the right ip address and port***"
     log.startLogging(sys.stdout)
     debug = True #u can switch the debug module by True of False
@@ -92,5 +111,8 @@ if __name__ == '__main__':
     factory.protocol = BroadcastServerProtocol
     factory.setProtocolOptions(allowHixie76 = True)
     listenWS(factory)
-    threading.Thread(target=readLog, args=(log_path,)).start()
+    set_exit_handler(on_exit)
+    log_thread = ReadLogThread(log_path)
+    log_thread.setDaemon(True)
+    log_thread.start()
     reactor.run()
